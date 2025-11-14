8edccffec00c49905fc7671d2bc096fac1fe6d2f
Gfarf <gfpalu@gmail.com>
2025-11-14 17:37:19 -0300

    simple search ok

--- cli/lib/keyword_search.py
+++ cli/lib/keyword_search.py
@@ -1,15 +1,27 @@
-from .utils import DEFAULT_SEARCH_LIMIT, load_movies, remove_punctuation
+from .utils import DEFAULT_SEARCH_LIMIT, load_movies, remove_punctuation, tokenize, remove_stop_word, stemming
 
 def search_command(query: str, limit: int = DEFAULT_SEARCH_LIMIT) -> list[dict]:
     movies_list = load_movies()
     query = preprocess_to_lower(query)
     query = remove_punctuation(query)
+    list_query = tokenize(query)
+    list_query = remove_stop_word(list_query)
+    list_query = stemming(list_query)
     result = []
     for movie in movies_list:
         movie1 = preprocess_to_lower(movie["title"])
         movie1 = remove_punctuation(movie1)
-        if movie1.find(query) != -1:
-            result.append(movie)
+        list_movie = tokenize(movie1)
+        list_movie = remove_stop_word(list_movie)
+        found = False
+        for token in list_movie:
+            if found:
+                break
+            for item in list_query:
+                if token.find(item) != -1:
+                    result.append(movie)
+                    found = True
+                    break
         if len(result) >= limit:
             break
     return result
--- cli/lib/utils.py
+++ cli/lib/utils.py
@@ -1,17 +1,25 @@
 import json
 import os
 import string
+from nltk.stem import PorterStemmer
 
 DEFAULT_SEARCH_LIMIT = 5
 
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 DATA_PATH = os.path.join(PROJECT_ROOT, "data", "movies.json")
+WORDS_DATA_PATH = os.path.join(PROJECT_ROOT, "data", "stopwords.txt")
 
 
 def load_movies() -> list[dict]:
     with open(DATA_PATH, "r") as f:
         data = json.load(f)
     return data["movies"]
+
+def load_stopwords() -> list:
+    with open(WORDS_DATA_PATH, "r") as f:
+        data = f.read()
+    data = data.splitlines()
+    return data 
 
 def remove_punctuation(enter: str) -> str:
     # Create a translation table that maps each punctuation character to None
@@ -20,3 +28,21 @@ def remove_punctuation(enter: str) -> str:
     # Use the translate method to remove punctuation
     clean_text = enter.translate(translator)
     return clean_text
+
+def tokenize(enter: str) -> list:
+    lista = enter.split()
+    filtered_list = [item for item in lista if item]
+    return filtered_list
+
+def remove_stop_word(enter: list) -> list:
+    stopwords = load_stopwords()
+    for item in enter:
+        if item in stopwords:
+            enter.remove(item)
+    return enter
+
+def stemming(enter: list) -> list:
+    stemmer = PorterStemmer()
+    for i, item in enumerate(enter):
+        enter[i] = stemmer.stem(item)
+    return enter
